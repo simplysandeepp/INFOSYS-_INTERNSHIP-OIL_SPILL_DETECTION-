719f060ca5b82c492e61fa2237755ed9b92c500c
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-10-22 00:57:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -734,7 +734,7 @@ def main():
     st.markdown("""
     <div class="hero-section">
         <span class="emoji-icon">🌊🌊</span>
-        <h1>HydroVexel - An AI-Powered Oil Spill Detection System</h1>
+        <h1>HydroVexel - an AI-Powered Oil Spill Detection System</h1>
         <p class="subtitle">
             Our system leverages cutting-edge Deep Learning and AI technologies to detect and analyze oil spills from satellite and aerial imagery with high speed and accuracy. Designed for environmental monitoring agencies, researchers, and response teams, it transforms raw imagery into actionable insights, helping protect marine ecosystems and coastal communities.
         </p>
