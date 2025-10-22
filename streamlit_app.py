6c8cf16a7cdea61ee13425ee193c91ed315d23a5
Sandeep Prajapati <139686638+simplysandeepp@users.noreply.github.com>
2025-10-22 17:40:47 +0530

    Update hero section text in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -327,10 +327,10 @@ def main():
     st.markdown("""
     <div class="hero-section">
         <span class="emoji-icon">🌊🌊</span>
-        <h1>HydroVexel⚡️</h1>
-        <h4>AI-Powered Oil Spill Detection System :) </h4>
+        <h1>HydroVexel1⚡️</h1>
+        <h4>AI-Powered Oil Spill Detection System 🤖 </h4>
                 <br>
-        <p style="font-family:Poppins,sans-serif;font-style:italic;font-weight:300;font-size:1.2rem;color:#ffffff;"> <b>AI-Powered</b> Eyes Safeguarding Our <b>Precious Oceans</b>✨️</p>
+        <p style="font-family:Poppins,sans-serif;font-style:italic;font-weight:300;font-size:1.2rem;color:#ffffff;"> <b>AI</b> Eyes Safeguarding Our <b>Precious Oceans</b>✨️</p>
         <p class="subtitle">
             Our system leverages cutting-edge Deep Learning and AI technologies to detect and analyze oil spills from satellite and aerial imagery with high speed and accuracy. Designed for environmental monitoring agencies, researchers, and response teams, it transforms raw imagery into actionable insights, helping protect marine ecosystems and coastal communities.
         </p>
